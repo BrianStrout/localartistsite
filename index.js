55389409addb5b3bdf6b62a80beb42d172c8963e
Brian J. Strout <brianjstrout@gmail.com>
2023-02-01 21:42:55 -0500

    rough dradft of landing page animation complete

--- index.js
+++ index.js
@@ -1,13 +1,32 @@
 const launch = document.getElementById("launch");
 let treeplots = Array.from(document.querySelectorAll(".plant"));
+let farmsurvey = Array.from(document.querySelectorAll(".farm"));
 
-console.log(treeplots);
-
+const blanket = document.getElementById("blanket");
+const titleTrack = document.getElementById("titleTrack");
+const interval = 50;
 const animateInTreescape = () => {
-  treeplots.forEach((tree, index) => {
-    console.log(treeplots[index]);
-    tree.classList.add("normalize");
-  });
+  setTimeout(() => {
+    titleTrack.classList.add("outlined");
+  }, 1000);
+  setTimeout(() => {
+    titleTrack.classList.add("lit-with-shadow");
+  }, 2400);
+
+  setTimeout(() => {
+    blanket.classList.add("transparent");
+  }, 3500);
+  setTimeout(() => {
+    farmsurvey.forEach(function (tree, index) {
+      tree.classList.add("grow");
+    });
+  }, 6000);
+
+  setTimeout(() => {
+    treeplots.forEach((tree, index) => {
+      tree.classList.add("normalize");
+    });
+  }, 5000);
 };
 
 launch.addEventListener("click", () => {
